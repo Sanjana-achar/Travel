421502d306149f3a270a799a0698c4741f1fe2a8
Sanjana-achar <138116009+Sanjana-achar@users.noreply.github.com>
2023-07-17 16:49:27 +0530

    Add files via upload

---
+++ Destination.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Destination = () => {
+return (
+<>
+<h1>Destination</h1>
+</>.
+<>
+<div>
+<h6>1. Taj mahal</h6>
+<img src="C:\Users\hp\OneDrive\Desktop\travel\Travel\imagessss\OIP (1).jpg"></img>
+</div>
+<div>
+<h6>4. Kerala</h6>
+</div>
+<div>
+<h6>5.Dandeli</h6>
+</div>
+<div>
+<h6>6. Yana</h6>
+</div>
+</>
+)
+};
+
+export default Destination;
