7736ba9dd462d34029d49bd275eede6f706d3b58
sanjana <acharsanjana895@gmail.com>
2023-07-27 18:07:18 +0530

    done

---
+++ src/About.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        
+        <>
+        <h1>Tourism</h1>
+            <h4>
+            <p>According to Ministry of Tourism, Over 6.19 million and 1.52 million foreign tourists arrived in India
+in 2022 & 2021 respectively compared to 10.93 million in 2019, representing a -44%
+degrowth.[4][5]India has bigger domestic tourist population so it's not dependent on foreign tourist
+much. This makes India the 22nd most visited nation in the world and 8th in Asia and Pacific .[6]
+Domestic tourist visits to all states and union territories numbered 1,036.35 million in 2012, an
+increase of 16.5% from 2011.[7] In 2012, National Geographic's Traveller magazine named the state
+Kerala as one of the "ten paradises of the world" and "50 must see destinations of a lifetime".[8] In
+2014, Tamil Nadu, Maharashtra and Uttar Pradesh were the most popular states for tourists.[9] Delhi,
+Mumbai, Chennai, Agra and Jaipur were the five most visited cities of India by foreign tourists during
+the year 2015. As of 2017, worldwide, Delhi is ranked 28th by the number of foreign tourist arrivals,
+while Mumbai is ranked 30th, Chennai 43rd, Agra 45th, Jaipur 52nd and Kolkata 90th.[10]
+
+The World Economic Forum's Travel and Tourism Development Index 2021, which replaced its
+previous Travel and Tourism Competitiveness Report, ranks India 54th out of 117 countries
+overall.[11] The last edition of the Travel and Tourism Competitiveness Report, published in 2019,
+had ranked India 34th out of 140 countries overall. The report ranked the price competitiveness of
+India's tourism sector 13th out of 140 countries. It mentioned that India has quite good air transport
+infrastructure (ranked 33rd), particularly given the country's stage of development, and reasonable
+ground and port infrastructure (ranked 28th). The country also scored high on natural resources
+(ranked 14th), and cultural resources and business travel (ranked 8th).[12] The World Tourism
+Organization reported that India's receipts from tourism during 2012 ranked 16th in the world, and
+7th among Asian and Pacific countries.[13]
+
+The Ministry of Tourism designs national policies for the development and promotion of tourism. In
+the process, the Ministry consults and collaborates with other stakeholders in the sector including
+various central ministries/agencies, state governments, union territories and private sector
+representatives. Concerted efforts are being made to promote niche tourism products such as rural,
+cruise, medical and eco-tourism.[14] The Ministry of Tourism maintains the Incredible India
+campaign focused on promoting tourism in India.Tourism is travel for pleasure or business, and the commercial activity of providing and supporting such travel. The World Tourism Organization defines tourism more generally, in terms which go "beyond the common perception of tourism as being limited to holiday activity only", as people "travelling to and staying in places outside their usual environment for not more than one consecutive year for leisure and not less than 24 hours, business and other purposes". Tourism can be domestic or international, and international tourism has both incoming and outgoing implications on a country's balance of payments.</p>
+</h4>
+        </>
+    );
+};
+
+export default About;
+
+
